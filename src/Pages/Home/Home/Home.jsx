b7f8b6588500fe7562de1aabf83c09464d8c9f1c
nokibh <nokibhowlader2003@gmail.com>
2024-05-17 10:13:40 +0600

    swiper slider

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,15 @@
 import Banner from '../Banner/Banner';
+import Category from '../Category';
 
 const Home = () => {
   return (
-    <div className="mt-4 mb-5">
-      <Banner></Banner>
+    <div>
+      <div className="mt-4 mb-5">
+        <Banner></Banner>
+      </div>
+      <div className="mt-4 mb-5">
+        <Category></Category>
+      </div>
     </div>
   );
 };
